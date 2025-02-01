cfb71f0bbcadb66b4119b232ed63f71298568dc7
skarha-a <mydarkestpearl@gmail.com>
2025-02-01 20:27:51 +0100

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,7 +8,8 @@ const bookmarks = [
       "hab": "https://habitica.com/",
       "uni": "https://idp.unicampania.it/idp/Authn/UserPassword",
       "gym": "https://www.notion.so/63234815ce13442a916031f2a79c616b?v=185cd9560dc08051ad2c000cd8c002ee",
-      "r/coolgithubprojects ": "https://www.reddit.com/r/coolgithubprojects ",
+      "x": "https://www.x.com/",
+      "yt": "https://www.youtube.com/",
     },
   },
   {
@@ -17,7 +18,8 @@ const bookmarks = [
       "snahp": "https://fora.snahp.eu/?__cf_chl_rt_tk=5bbmruXdjRjw_0mO85ImZSe7GdrTdrCn82RKUE0BcTk-1738406398-1.0.1.1-n_ZcfeMUHcv.hZ2B5TQJXnRNcYOTGLMrOb9mhLeQUpI",
       "ru_t": "https://rutracker.net/forum/index.php",
       "nya": "https://nyaa.land/",
-      "js > bundlephobia": "https://bundlephobia.com/",
+      "gpt": "https://chatgpt.com/",
+      "ds": "https://chat.deepseek.com/",
     },
   },
 ]
